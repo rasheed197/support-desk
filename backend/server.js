fe8d5c368e1358b586d83bc69efe794b16cb6bd9
rasheed197 <tiamiyurasheed.tr@gmail.com>
2026-09-25 09:48:32 +0100

    fix 405 error 2

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express') // To create routes
+const cors = require('cors');
 const dotenv = require('dotenv').config() // For support of .env file
 const colors = require('colors')
 
@@ -11,21 +12,17 @@ connectDB()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 8000
 
-const cors = require('cors');
-app.use(cors({
-  origin: 'https://support-desk-green-eight.vercel.app', // Your Vercel frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true
-}));
-
 const app = express()
 
 
 // Middlewares
 app.use(express.json()); // Whatever request we get will be passed using the json
 app.use(express.urlencoded({extended: false})) // For form-urlencoded format
-
-
+app.use(cors({
+  origin: 'https://support-desk-green-eight.vercel.app', // Your Vercel frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
 
 // API endpoints
 app.use('/api/users', require('./routes/userRoutes'))
