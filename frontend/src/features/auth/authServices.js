2bd808ae5f6c11baf82f4a36c89e09d2f2a5367b
rasheed197 <tiamiyurasheed.tr@gmail.com>
2026-09-25 10:28:50 +0100

    fix 405 error 6

--- frontend/src/features/auth/authServices.js
+++ frontend/src/features/auth/authServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "/api/users";
+const API_URL = import.meta.env.VITE_API_URL + "/api/users/" || "/api/users/";
 
 // Register user
 const register = async (userData) => {
@@ -15,7 +15,7 @@ const register = async (userData) => {
 
 // Login user
 const login = async (userData)=>{
-    const response = await axios.post(API_URL + '/login', userData)
+    const response = await axios.post(API_URL + 'login', userData)
 
     if(response.data){
         localStorage.setItem('user', JSON.stringify(response.data))
--- frontend/src/features/notes/noteServices.js
+++ frontend/src/features/notes/noteServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "/api/tickets/";
+const API_URL = import.meta.env.VITE_API_URL + "/api/tickets/" || "/api/tickets/";
 
 // Get ticket notes
 const getNotes = async (ticketId, token) => {
